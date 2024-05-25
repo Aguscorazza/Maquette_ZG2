8e07d6d66b94df5e433c06b112614286f5bbe4cc
AgustinCorazza <corazzaagustin@gmail.com>
2024-05-25 14:49:06 +0200

    Finished P2 Presentation code

--- Code_arduino/mainArduino/mainArduino.ino
+++ Code_arduino/mainArduino/mainArduino.ino
@@ -1,18 +1,27 @@
+#include <util/atomic.h>
+
+#define F_CPU 16000000
+
+// UART DEFINITIONS
 #define UART_BAUDRATE     38400
 #define TX_BUFFER_SIZE    20
 #define RX_BUFFER_SIZE    20
 
-#define FREQ_TIM1         10        //Hz
-#define FREQ_PWM          25000     // Hz
 
+// ARDUINO PIN DEFINITIONS
+#define LED_PIN           13
+#define DIR_MOTOR_PIN     12
+#define BRAKE_MOTOR_PIN   9
+#define PWM_MOTOR_PIN     3
+#define ENCODER_A_PIN     2
+
+
+// CONTROL DEFINITIONS
 #define MAX_TENSION       12    // V
 #define ENCODER_CPR       64    // Encoder Counts per Revolution (CPR)
 #define MOTOR_GEAR_RATIO  50
-#define MAX_I_ERROR       1000
+#define MAX_I_ERROR       100
 #define pi                3.1416
-
-#define Kp 0.001
-#define Ki 0.0001
 #define Kt 38.4615            // Motor Torque Constant
 #define Ke (Kt)
 #define Ra 2.2
@@ -20,22 +29,29 @@
 #define R 72.2566             // 2*PI*fc*La // fc = 5000hz
 #define bm 0.36728            // No-load current * Kt / No-load speed 
 
+// globals
+long prevT = 0;
+int posPrev = 0;
+float v1Prev = 0;
+volatile int pos_i=0;                     // Number of counts of the encoder signal (used to calcule motor speed)
+float deltaT = 0.0;
+float eintegral = 0;
 
 typedef struct{
-  int duration;                 // Simulation duration
-  int mode;                     // 0: Oscillation Libre - 1: Oscillation forcee
-  double commanded_motor_speed;  // Motor speed (consigne) [rad/s]
+  int mode=0;                       // 0: Oscillation Libre - 1: Oscillation forcee
+  float commanded_motor_speed=0.0;  // Motor speed (consigne) [rpm]
   
-  float i_error;
-  double control_current, control_torque, control_tension;
-  uint8_t control_rpm;
-  uint16_t output_dutycycle;
+  float Kp = 20.0;
+  float Ki = 10.0;
   
-  int encoder_count;            // Number of counts of the encoder signal (used to calcule motor speed)
-  double measured_motor_speed;   // Motor speed (calculation) [rad/s]
-  int measured_motor_speed_rpm;
-  double measured_motor_current;  // Motor current [A]
-  int measured_position;        // Position (calculation)
+  unsigned int Te = 50;                      // Période d'échantillonnage [ms]
+  
+  float measured_motor_speed=0.0;           // Motor Speed [rpm]
+  float measured_motor_current=0.0;         // Motor current [A]
+  int measured_position=0;                  // Position [m]
+  
+  bool simulation_running=false;
+  uint16_t T1_prescaler = 0;
 } Simulation;
 
 char TX_buffer[TX_BUFFER_SIZE];
@@ -44,109 +60,81 @@ char TEST_BUFFER[TX_BUFFER_SIZE];
 volatile int rx_index=0, command_flag=0;
 
 Simulation simulation;
-int start_simulation_flag = 0; // Set this variable to start the simulation
-int TIM1_OVF_counter = 0;
 
-volatile bool newData = false;
-char receivedData;
-
-void PWM_init() {
-  // Configure TIMER2 as PWM (Motor Input PWM)
-  // Pin configuration
-  DDRD |= (1<<DDD3);    // OUTPUT - Arduino pin 3 - Motor PWM (MOTOR A) (OC2B)
-  
-  // Output Mode (OC2B) Configuration (FAST PWM - Clear OC2B on compare match, set OC2B at BOTTOM)
-  TCCR2A |= (1<<COM2B1);
-  TCCR2A &= ~(1<<COM2B0);
-  
-  // Timer Operation Mode (FastPWM - TOP = OCR2A)
-  TCCR2A |= (1<<WGM20);
-  TCCR2A |= (1<<WGM21); 
-  TCCR2B |= (1<<WGM22);
-  
-  TCNT2 = 0;
-  // Configure Prescaler x8 (base freq = 16MHz / 8 = 2MHz)
-  TCCR2B &= ~(1<<CS22);
-  TCCR2B |= (1<<CS21);
-  TCCR2B &= ~(1<<CS20);
-  
-  // Configure TOP = OCR2A
-  OCR2A = (F_CPU / (8 * FREQ_PWM)) - 1; // With Prescaler x8
-}
-
-void PWM_set_duty(int duty) {
-  if(duty>100){
-    duty = 100;
+void setMotor(int dir, int pwmVal){
+  digitalWrite(BRAKE_MOTOR_PIN,LOW); 
+  analogWrite(PWM_MOTOR_PIN,pwmVal); // Motor speed
+  if(dir == 1){ 
+    // Turn one way
+    digitalWrite(DIR_MOTOR_PIN,HIGH);
   }
-  if(duty>=0){
-    analogWrite(3, duty);
+  else if(dir == -1){
+    // Turn the other way
+    digitalWrite(DIR_MOTOR_PIN,LOW);
+  }
+  else{
+    // Or dont turn
+    digitalWrite(BRAKE_MOTOR_PIN,HIGH);   
   }
 }
-
-void PWM_on() {
-  TCNT2 = 0;
-  // Configure Prescaler x8 (base freq = 16MHz / 8 = 2MHz)
-  TCCR2B &= ~(1<<CS22);
-  TCCR2B |= (1<<CS21);
-  TCCR2B &= ~(1<<CS20);
-  
+void setMotor(int pwmVal){
+  analogWrite(PWM_MOTOR_PIN,pwmVal); // Motor speed
 }
 
-void PWM_off() {
-  // Turn off TIMER
-  TCCR2B &= ~(1<<CS22);
-  TCCR2B &= ~(1<<CS21);
-  TCCR2B &= ~(1<<CS20);
-  TCNT2 = 0;
-}
-
-void PIController(float setpoint){
-  float speed_error = setpoint-simulation.measured_motor_speed;
-  simulation.i_error += speed_error; // Integral error
-  
-  // Anti-windup mechanism to limit the integral term
-  if (simulation.i_error > MAX_I_ERROR) {
-      simulation.i_error = MAX_I_ERROR;
-  } else if (simulation.i_error < -MAX_I_ERROR) {
-      simulation.i_error = -MAX_I_ERROR;
-  }
-
-  simulation.control_torque = Kp * speed_error + Ki * simulation.i_error;
-  simulation.control_current = (simulation.control_torque + bm * simulation.measured_motor_speed)/Kt;
-  simulation.control_tension = R*(simulation.control_current - simulation.measured_motor_current) + Ke * simulation.measured_motor_speed + Ra * simulation.measured_motor_current;
-
-  simulation.output_dutycycle = (simulation.control_tension * 100) / (MAX_TENSION);
-  PWM_set_duty(simulation.output_dutycycle);
-}
-
-void TIMER_init(){
+void TIMER1_init(bool enable, unsigned int T){
   // Configure TIMER 1
   // This timer will periodically send data to the PC via the UART
   // Reset Prescaler (Stop Timer)
-  TCCR1B &= ~(1<<CS10);
-  TCCR1B &= ~(1<<CS11);
-  TCCR1B &= ~(1<<CS12);
-  
-  // Operation mode (FastPWM - TOP: OCR1A)
-  TCCR1A |= (1<<WGM10);
-  TCCR1A |= (1<<WGM11);
-  TCCR1B |= (1<<WGM12);
-  TCCR1B |= (1<<WGM13);
+  TCCR1B &= ~(7<<CS10);
+  TCCR1A = 0;
+  TCNT1 = 0;
+  if(enable) {
+    
+    float period = (float) (T/1000.0);
+    float max_period = (float) (pow(2,16)/F_CPU);
+    unsigned int prescaler_T1 = 0;
+    if(period <= max_period) prescaler_T1=1;
+    else if(period <= 8*max_period) prescaler_T1=8;
+    else if(period <= 64*max_period) prescaler_T1=64;
+    else if(period <= 256*max_period) prescaler_T1=256;
+    else if(period <= 1024*max_period) prescaler_T1=1028;
+    else prescaler_T1=0; // Desactivate TIMER in case excessive period
 
-  // Set TOP Value (Check FREQ_TIM_1 in config.h)
-  OCR1A = (F_CPU/(1024*FREQ_TIM1)) - 1; //Using a x1024 Prescaler
+    
+    switch(prescaler_T1) {
+      case 1: //prescaler x1
+      simulation.T1_prescaler |= (1<<CS10);
+      break;
+      case 8: //prescaler x8
+      simulation.T1_prescaler |= (2<<CS10);
+      break;
+      case 64: //prescaler x64
+      simulation.T1_prescaler |= (3<<CS10);
+      break;
+      case 256: //prescaler x256
+      simulation.T1_prescaler |= (4<<CS10);
+      break;
+      case 1028: //prescaler x1024
+      simulation.T1_prescaler |= (5<<CS10);
+      break;
+      default:
+      case 0: //Timer off
+      simulation.T1_prescaler &= ~(7<<CS10);
+      break;
+    }
+    
+    OCR1A = (uint16_t)(((period*F_CPU)/prescaler_T1)-1);
+  }
+  TCCR1B |= (1 << WGM12);
   
   // Enable Timer Overflow Interrupt
-  TIFR1 |= (1<<TOV1); // Clear flag
-  TIMSK1 |= (1<<TOIE1);
+  TIMSK1 |= (1 << OCIE1A);
 }
 
 void timer_on(){
   // Prescaler x1024
   TCNT1 = 0;
-  TCCR1B |= (1<<CS10);
-  TCCR1B &= ~(1<<CS11);
-  TCCR1B |= (1<<CS12);
+  TCCR1B |= simulation.T1_prescaler;
 }
 
 void timer_off(){
@@ -157,17 +145,6 @@ void timer_off(){
   TCNT1 = 0;
 }
 
-/**
- * Initialisation de l'UART (Universal Asynchronous Receiver-Transmitter) avec les paramètres spécifiés.
- * 
- * @param baud_rate Taux de bauds de communication.
- * @param intRx Activation de l'interruption de réception (1 pour activer, 0 pour désactiver).
- * @param intTx Activation de l'interruption de transmission (1 pour activer, 0 pour désactiver).
- * 
- * Cette fonction configure l'UART pour le fonctionnement souhaité avec les paramètres spécifiés.
- * Elle initialise le registre de bauds, active la réception et/ou la transmission selon les paramètres
- * fournis, et active les interruptions de réception et/ou de transmission si spécifiées.
- */
 void UART_init(uint64_t baud_rate, uint8_t intRx, uint8_t intTx) {
   UBRR0 = F_CPU/16/baud_rate-1; // Configuration du registre de bauds
   UCSR0A &= ~(1<<U2X0); // Assure que le bit U2X0 est à 0 (vitesse normale)
@@ -201,36 +178,43 @@ void get_command(const char *command){
     case 'S': //Start Simulation
       start_simulation_command(command, &simulation);
       break;
+    case 'E':
+      stop_simulation_command(command, &simulation);
+      break;
+    case 'P':
+      set_parameters_command(command, &simulation);
+      break;
     default:
       break;
   }
 }
 
 void start_simulation_command(const char *command, Simulation *simulation){
-  sscanf(command, "S_%d_%d_%d", &(simulation->duration), &(simulation->mode), &(simulation->commanded_motor_speed));
-  simulation->commanded_motor_speed = 2.0*pi*simulation->commanded_motor_speed/60.0; // rad/s
-  // Start timer during the specified duration (simulation.duration)
-
+  sscanf(command, "S_%d_%d", &(simulation->mode), &(simulation->commanded_motor_speed));
+  simulation->commanded_motor_speed = 2.0*pi*simulation->commanded_motor_speed/60.0; // [rpm]-->[rad/s]
+  
   switch(simulation->mode){
-    case 1: //Oscillation forcée
-      
-      digitalWrite(9, LOW);
-      PWM_on();
+    case 1: //Oscillation forcée      
+      digitalWrite(BRAKE_MOTOR_PIN, LOW);
       break;
       
     case 0: // Oscillation libre
     default:
-      digitalWrite(9,HIGH);
-      PWM_set_duty(0);
-      PWM_off();
+      digitalWrite(BRAKE_MOTOR_PIN,HIGH);
+      setMotor(0);
       break;
   }   
-  
-  digitalWrite(13, HIGH);
-  TIM1_OVF_counter = 0;
+  // Start simulation
+  simulation->simulation_running=true;
   timer_on();
 }
 
+void stop_simulation_command(const char *command, Simulation *simulation){
+  simulation->simulation_running=false;
+  timer_off();
+  // Arrêter le moteur
+  setMotor(0);
+}
 
 int get_distance(){
   int t = pulseIn(8, HIGH);//branché sur le pin 5
@@ -240,52 +224,95 @@ int get_distance(){
     }
 }
 
+void set_parameters_command(const char *command, Simulation *simulation){
+  sscanf(command, "P%d-I%d-T%d", &(simulation->Kp), &(simulation->Ki), &(simulation->Te));
+}
+
 void setup() {
   noInterrupts();                                               // Désactive les interruptions pendant la configuration initiale
   UART_init(UART_BAUDRATE, 1, 0);                               // Initialise la communication UART avec le taux de bauds spécifié
-  TIMER_init();                                                 // Initialise les timers
-  PWM_init();
+
+  TIMER1_init(true, simulation.Te);
   // Configuration des broches
-  pinMode(13, OUTPUT); // Configure la broche 13 en sortie
-  pinMode(12, OUTPUT);                                          // Broche Arduino réservée pour le sens de rotation du moteur A
-  pinMode(9, OUTPUT);                                           // Broche Arduino réservée pour le freinage du moteur A
-  pinMode(3, OUTPUT);                                           // Broche PWM pour le contrôle de la vitesse du moteur A
-  pinMode(2, INPUT);                                            // Broche d'entrée pour l'encodeur avec résistance de pull-up activée
-  attachInterrupt(digitalPinToInterrupt(2), encoderISR, RISING); // Associe la fonction d'interruption encoderISR à la broche d'encodeur, déclenchée sur front-montant
+  pinMode(LED_PIN, OUTPUT);                                          // Configure la broche 13 en sortie
+  pinMode(DIR_MOTOR_PIN, OUTPUT);                                          // Broche Arduino réservée pour le sens de rotation du moteur A
+  pinMode(BRAKE_MOTOR_PIN, OUTPUT);                                           // Broche Arduino réservée pour le freinage du moteur A
+  pinMode(PWM_MOTOR_PIN, OUTPUT);                                           // Broche PWM pour le contrôle de la vitesse du moteur A
+  pinMode(ENCODER_A_PIN, INPUT);                                            // Broche d'entrée pour l'encodeur avec résistance de pull-up activée
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A_PIN), encoderISR, RISING); // Associe la fonction d'interruption encoderISR à la broche d'encodeur, déclenchée sur front-montant
 
   // État initial des broches
-  digitalWrite(12, HIGH); // Le moteur A tourne dans le sens normal
-  digitalWrite(9, HIGH);  // Activation du frein moteur A
-  analogWrite(3, 0);      // Pas de vitesse pour le moteur A (PWM)
-  
+  digitalWrite(DIR_MOTOR_PIN, HIGH); // Le moteur A tourne dans le sens normal
+  digitalWrite(BRAKE_MOTOR_PIN, HIGH);  // Activation du frein moteur A
+  analogWrite(PWM_MOTOR_PIN, 0);      // Pas de vitesse pour le moteur A (PWM)
+
   interrupts(); // Réactive les interruptions après la configuration initiale
 
   put_string("UART OK"); // Envoie un message indiquant que la configuration UART est terminée
 }
 
 void loop() {
+  if(simulation.simulation_running==true){
+    if((micros()-prevT)/1.0e3>=(simulation.Te-deltaT)){
+      // Mesurer la distance de la masse
+      simulation.measured_position = get_distance();
+      
+      // read the encoder counts in an atomic block
+      // to avoid potential misreads
+      int pos = 0;
+      ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+        pos = pos_i;
+      }
+      
+      long currT = micros();
+      deltaT = ((float) (currT-prevT))/1.0e6;
+      float velocity1 = (pos - posPrev)/deltaT;
+      posPrev = pos;
+      prevT = currT;
+      
+      // Convert count/s to RPM
+      float v1 = velocity1/800.0*60.0;
+      // Low-pass filter (25 Hz cutoff)
+      simulation.measured_motor_speed = (0.854*simulation.measured_motor_speed + 0.0728*v1 + 0.0728*v1Prev)*2.0*pi/60.0; // [rpm]-->[rad/s]
+      v1Prev = v1;
+      
+      // Measure the motor current
+      simulation.measured_motor_current = map(analogRead(A0), 0, 1024, 0, 5000)*2.0/(1000.0*3.3);
+        
+      if(simulation.mode==1){
+        // Compute the control signal u
+        float e = simulation.commanded_motor_speed-simulation.measured_motor_speed;
+        eintegral = eintegral + e*deltaT;
+
+        if(eintegral > MAX_I_ERROR){
+          eintegral = MAX_I_ERROR;
+        } else if(eintegral< -MAX_I_ERROR){
+          eintegral = -MAX_I_ERROR;
+        }
+        
+        float u = simulation.Kp*e + simulation.Ki*eintegral;
+        // Set the motor speed and direction
+        int dir = 1;
+        if (u<0){
+          dir = -1;
+        }
+        int pwr = (int) fabs(u);
+        if(pwr > 255){
+          pwr = 255;
+        }
+        setMotor(dir,pwr);
+      }
+    }
+  }
+
 }
 
-/**
- * Fonction d'interruption pour la gestion des compteurs d'encodeurs.
- * 
- * Cette fonction est appelée à chaque fois qu'une interruption est générée par l'encodeur.
- * Son rôle est d'incrémenter le compteur d'encodeurs pour suivre le nombre d'impulsions reçues.
- */
+
 void encoderISR(){ 
-  simulation.encoder_count++; // Incrément du compteur d'encodeurs
+  pos_i++; // Incrément du compteur de l'encodeur
 }
-
-
-/**
- * Routine de service d'interruption (ISR) déclenchée lors de la réception de données sur l'USART.
- * 
- * Cette ISR est activée lorsqu'un caractère est reçu sur le port de communication série (USART).
- * Elle gère la réception de commandes, délimitées par les caractères '#' et '%'. Elle stocke les
- * caractères reçus dans un tampon circulaire jusqu'à ce que la commande soit complète, puis elle
- * passe la commande complète à la fonction de traitement des commandes.
- */
-ISR(USART_RX_vect){  
+ 
+ISR(USART_RX_vect){
   char RX_received_char; // Caractère reçu depuis l'USART
   RX_received_char = UDR0; // Lecture du caractère reçu
   
@@ -319,46 +346,8 @@ ISR(USART_RX_vect){
   }
 }
 
-
-/**
- * Routine de service d'interruption (ISR) déclenchée par le dépassement de décompte du TIMER1.
- * 
- * Cette ISR est activée lorsque le TIMER1 dépasse sa valeur de décompte maximale. Elle est utilisée
- * pour effectuer plusieurs tâches périodiques, y compris le calcul de la vitesse du moteur, la mesure
- * de la distance parcourue, l'envoi de données via la communication série, et l'arrêt des timers
- * lorsque la simulation est terminée.
- */
-ISR(TIMER1_OVF_vect){ // Freq = FREQ_TIM_1
-  // Basculer la broche 13
-  //digitalWrite(13, !digitalRead(13));
-  
-  // Calculer la vitesse du moteur
-  simulation.measured_motor_speed_rpm = (simulation.encoder_count * FREQ_TIM1 * 60)/(ENCODER_CPR*MOTOR_GEAR_RATIO); // RPM
-  simulation.measured_motor_speed = simulation.measured_motor_speed_rpm * 2.0 * pi / 60.0; 
-  simulation.encoder_count = 0;
-
-  // Measure the motor current
-  simulation.measured_motor_current = map(analogRead(A0), 0, 1024, 0, 5000)*2.0/(1000.0*3.3);
-  
-  if(simulation.mode==1){
-    PIController(simulation.commanded_motor_speed);
-  }
-  // Mesurer la distance parcourue
-  simulation.measured_position = get_distance();
-
-  // Envoyer les données
-  sprintf(TX_buffer, "#P%d-V%d-I%d%", simulation.measured_position, simulation.measured_motor_speed_rpm, simulation.measured_motor_current);
+ISR(TIMER1_COMPA_vect) {
+  // Envoyer les données périodiquement
+  sprintf(TX_buffer, "#P%d-V%d-I%d%", simulation.measured_position, simulation.measured_motor_speed, simulation.measured_motor_current);
   put_string(TX_buffer);
-
-  // Désactiver les timers lorsque la simulation est terminée
-  if(TIM1_OVF_counter/FREQ_TIM1>=simulation.duration){
-    timer_off();
-    // Arrêter le moteur
-    PWM_set_duty(0);
-    PWM_off();
-    TIM1_OVF_counter = 0;
-    digitalWrite(13, LOW);
-  } else{
-    TIM1_OVF_counter++;
-  }
 }
